5ff0ce891a9dff237babaa7e04254ec709d2a229
Thien <ducthien190203@email.com>
2025-08-28 16:23:00 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Counter2 from "./components/Counter2";
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Custom Hook Counter</h1>
+      <h1>new title Custom Hook Counter</h1>
       <Counter1 />
       <Counter2 />
     </div>
